1c29367210d917e9d454893d0f7c7fe6f8b16b65
adriantd3 <adriantorremocha@hotmail.com>
2024-11-04 20:38:44 +0100

    Comments microservice test

---
+++ src/comments/src/openapi_server/apis/default_api_base.py
@@ -0,0 +1,55 @@
+# coding: utf-8
+
+from typing import ClassVar, Dict, List, Tuple  # noqa: F401
+
+from openapi_server.models.comment import Comment
+from openapi_server.models.comment_list_response import CommentListResponse
+from openapi_server.models.new_comment import NewComment
+
+
+class BaseDefaultApi:
+    subclasses: ClassVar[Tuple] = ()
+
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        BaseDefaultApi.subclasses = BaseDefaultApi.subclasses + (cls,)
+    async def delete_comment(
+        self,
+        comment_id: str,
+    ) -> None:
+        """Deletes an article&#39;s comment"""
+        ...
+
+
+    async def get_article_comments(
+        self,
+        article_id: str,
+        order: str,
+        limit: int,
+        offset: int,
+        creation_date: str,
+    ) -> CommentListResponse:
+        """Retrieves all comments from an articles"""
+        ...
+
+
+    async def get_users_comments(
+        self,
+        user_id: str,
+        article_id: str,
+        order: str,
+        limit: int,
+        offet: int,
+        creation_date: str,
+    ) -> CommentListResponse:
+        """Retrieves all comments from an user"""
+        ...
+
+
+    async def post_comment(
+        self,
+        article_id: str,
+        new_comment: NewComment,
+    ) -> Comment:
+        """Posts a new comment in an article"""
+        ...
---
+++ src/comments/tests/test_default_api.py
@@ -0,0 +1,89 @@
+# coding: utf-8
+
+from fastapi.testclient import TestClient
+
+
+from openapi_server.models.comment import Comment  # noqa: F401
+from openapi_server.models.comment_list_response import CommentListResponse  # noqa: F401
+from openapi_server.models.new_comment import NewComment  # noqa: F401
+
+
+def test_delete_comment(client: TestClient):
+    """Test case for delete_comment
+
+    delete a comment
+    """
+
+    headers = {
+    }
+    # uncomment below to make a request
+    #response = client.request(
+    #    "DELETE",
+    #    "/comments/{comment_id}".format(comment_id='comment_id_example'),
+    #    headers=headers,
+    #)
+
+    # uncomment below to assert the status code of the HTTP response
+    #assert response.status_code == 200
+
+
+def test_get_article_comments(client: TestClient):
+    """Test case for get_article_comments
+
+    get an article's comments
+    """
+    params = [("order", 'recent'),     ("limit", 20),     ("offset", 0),     ("creation_date", '2013-10-20')]
+    headers = {
+    }
+    # uncomment below to make a request
+    #response = client.request(
+    #    "GET",
+    #    "/comments/articles/{article_id}".format(article_id='article_id_example'),
+    #    headers=headers,
+    #    params=params,
+    #)
+
+    # uncomment below to assert the status code of the HTTP response
+    #assert response.status_code == 200
+
+
+def test_get_users_comments(client: TestClient):
+    """Test case for get_users_comments
+
+    get an user's comments
+    """
+    params = [("article_id", 'article_id_example'),     ("order", 'recent'),     ("limit", 20),     ("offet", 0),     ("creation_date", '2013-10-20')]
+    headers = {
+    }
+    # uncomment below to make a request
+    #response = client.request(
+    #    "GET",
+    #    "/comments/users/{user_id}".format(user_id='user_id_example'),
+    #    headers=headers,
+    #    params=params,
+    #)
+
+    # uncomment below to assert the status code of the HTTP response
+    #assert response.status_code == 200
+
+
+def test_post_comment(client: TestClient):
+    """Test case for post_comment
+
+    post a comment
+    """
+    new_comment = {"author_id":"046b6c7f-0b8a-43b9-b35d-6489e6daee91","body":"body"}
+
+    headers = {
+    }
+    # uncomment below to make a request
+    #response = client.request(
+    #    "POST",
+    #    "/comments/articles/{article_id}".format(article_id='article_id_example'),
+    #    headers=headers,
+    #    json=new_comment,
+    #)
+
+    # uncomment below to assert the status code of the HTTP response
+    #assert response.status_code == 200
+
