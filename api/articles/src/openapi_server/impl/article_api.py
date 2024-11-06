3b8f6133cbd7e7dcd93a7d67c0a018808369a32d
Victor Perez <vparmenta@uma.es>
2024-11-06 15:46:21 +0100

    Get Article by Id incomplete
    It returns the first articleversion with the given id, and it must return the ArticleVersion whose article_id = the given id.

--- api/articles/src/openapi_server/impl/article_api.py
+++ api/articles/src/openapi_server/impl/article_api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from motor.motor_asyncio import AsyncIOMotorClient
 from bson import ObjectId
 from openapi_server.apis.default_api_base import BaseDefaultApi
@@ -12,7 +12,32 @@ class ArticleAPI(BaseDefaultApi):
     def __init__(self):
         super().__init__()
 
-    async def get_article_by_id(self, id: str,) -> ArticleVersion:
-        article = await mongodb["article"].find_one({"_id": ObjectId(id)})
+    async def get_article_by_id(self, id: str,):
+        pipeline = [
+            {"$match": {"_id": ObjectId(id)}},
+            {"$addFields": {
+                "id": {"$toString": "$_id"},
+                "article_id": {"$toString": "$article_id"},
+                "author.id": {"$toString": "$author._id"},
+                "tags": {
+                    "$map": {
+                        "input": "$tags",
+                        "as": "tag",
+                        "in": {
+                            "id": {"$toString": "$$tag._id"},
+                            "tag": "$$tag.tag"
+                        }
+                    }
+                }
+            }},
+            {"$unset": ["_id", "author._id", "tags._id"]}  # Quita los campos _id originales
+        ]
 
-        return article
+        # Ejecutar la consulta de agregación
+        article = await mongodb["article_version"].aggregate(pipeline).to_list(length=1)
+
+        # Verificar si se encontró el artículo
+        if not article:
+            raise HTTPException(status_code=404, detail="Article not found")
+
+        return article[0]  # El resultado es una lista, así que se toma el primer elemento
