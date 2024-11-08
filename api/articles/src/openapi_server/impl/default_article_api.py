00feda98de97e9eb7c8ab5065539d96b63450f92
JoseBueno30 <124668506+JoseBueno30@users.noreply.github.com>
2024-11-08 17:48:17 +0100

    search semi functional

--- api/articles/src/openapi_server/impl/default_article_api.py
+++ api/articles/src/openapi_server/impl/default_article_api.py
@@ -1,7 +1,9 @@
+from datetime import datetime
 
 from motor.motor_asyncio import AsyncIOMotorClient
 from bson import ObjectId
 from openapi_server.apis.default_api_base import BaseDefaultApi
+from openapi_server.models import tag
 from openapi_server.models.article_list import ArticleList
 
 mongodb_client = AsyncIOMotorClient("mongodb+srv://lawiki:lawiki@lawiki.vhgmr.mongodb.net/")
@@ -167,7 +169,7 @@ class DefaultArticleAPI(BaseDefaultApi):
             },
             {
                 "$sort": {
-                    "creation_date": 1 if order == "asc" else -1
+                    "creation_date": -1 if order == "recent" else 1
                 }
             },
             {
@@ -217,6 +219,121 @@ class DefaultArticleAPI(BaseDefaultApi):
         ]
 
         articles = await mongodb["article"].aggregate(pipeline).to_list()
+
+        if not articles[0]:
+            raise Exception
+
+        return articles[0]
+
+    async def search_articles(
+        self,
+        wiki_id: str,
+        name: str,
+        tags: list[str],
+        offset: int,
+        limit: int,
+        order: str,
+        creation_date: str,
+        author_name: str,
+        editor_name: str,
+    ) -> ArticleList:
+        """Get a list of Articles from a given Wiki that match a keyword string. Results can by filtered by tags, sorted by different parameters and support pagination."""
+        matching_variables = {}
+        if wiki_id is not None:
+            matching_variables["wiki_id"] = ObjectId(wiki_id)
+        if name is not None:
+            matching_variables["title"] = name
+        if creation_date is not None:
+            dates = creation_date.split("-")
+            if len(dates) == 1:
+                matching_variables["creation_date"] = datetime.strptime(dates[0], "%Y/%m/%d")
+            elif len(dates) == 2:
+                matching_variables["creation_date"] = {
+                    "$gte": datetime.strptime(dates[0], "%Y/%m/%d"),
+                    "$lte": datetime.strptime(dates[1], "%Y/%m/%d")
+                }
+        if author_name is not None:
+            matching_variables["author.name"] = author_name
+        if editor_name is not None:
+            matching_variables["versions.author.name"] = editor_name
+
+        if tags is not None:
+            tag_ids = [ObjectId(tag["id"]) for tag in tags]
+            matching_variables["tags._id"] = {"$all": tag_ids}
+
+        total_count = await mongodb['article'].count_documents(matching_variables)
+
+        sorting_variables = {"rating" : -1}
+
+        if order == "recent":
+            sorting_variables["creation_date"] = {-1}
+        elif order == "oldest":
+            sorting_variables["creation_date"] = {1}
+        elif order == "popular":
+            sorting_variables["rating"] = {-1}
+        elif order == "unpopular":
+            sorting_variables["rating"] = {1}
+
+        group_articles_pipeline = [
+            {
+                "$group": {
+                    "_id": None,
+                    "articles": {
+                        "$push": "$$ROOT"
+                    }
+                }
+            },
+            {
+                "$project": {
+                    "_id": 0,
+                }
+            }
+        ]
+
+        print("AAAAAAAA")
+
+
+        #TODO: arreglar el total
+        pagination_pipeline = [
+            {
+                "$addFields": {
+                    "total": total_count,
+                    "offset": offset,
+                    "limit": limit,
+                    "next": {
+                        "$cond": {
+                            "if": {"$lt": [offset + limit, total_count]},
+                            "then": f"/articles/?offset={offset + limit}&limit={limit}",
+                            "else": None
+                        }
+                    },
+                    "previous": {
+                        "$cond": {
+                            "if": {"$gt": [offset, 0]},
+                            "then": f"/articles/?offset={max(offset - limit, 0)}&limit={limit}",
+                            "else": None
+                        }
+                    },
+                }
+            }
+        ]
+
+        query_pipeline = [
+            {"$match": matching_variables},
+            {"$sort": sorting_variables},
+            {"$skip": offset},
+            {"$limit": limit},
+            *transform_article_ids_pipeline,
+            *group_articles_pipeline,
+            *pagination_pipeline
+        ]
+
+        print(query_pipeline)
+
+        articles = await mongodb["article"].aggregate(query_pipeline).to_list()
+
+        print(articles)
+        print(articles[0])
 
         if not articles[0]:
             raise Exception
