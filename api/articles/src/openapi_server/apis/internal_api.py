4372d787ed9c6644bf05b72c25c1e6ef2704f951
José Ángel Bueno Ruiz <jabr3830@gmail.com>
2024-11-08 12:23:18 +0100

    internal methods unflagged as internal so they are generated

--- api/articles/src/openapi_server/apis/internal_api.py
+++ api/articles/src/openapi_server/apis/internal_api.py
@@ -23,6 +23,8 @@ from fastapi import (  # noqa: F401
 )
 
 from openapi_server.models.extra_models import TokenModel  # noqa: F401
+from openapi_server.models.id_ratings_body import IdRatingsBody
+from openapi_server.models.id_tags_body import IdTagsBody
 
 
 router = APIRouter()
@@ -30,6 +32,28 @@ router = APIRouter()
 ns_pkg = openapi_server.impl
 for _, name, _ in pkgutil.iter_modules(ns_pkg.__path__, ns_pkg.__name__ + "."):
     importlib.import_module(name)
+
+
+@router.put(
+    "/articles/{id}/tags",
+    responses={
+        204: {"description": "No Content, tags assigned"},
+        400: {"description": "Bad Request, invalid parameters format"},
+        403: {"description": "Forbidden"},
+        404: {"description": "Artcile Not Found"},
+    },
+    tags=["internal"],
+    summary="Assign Tags",
+    response_model_by_alias=True,
+)
+async def assign_article_tags(
+    id: str = Path(..., description=""),
+    id_tags_body: IdTagsBody = Body(None, description=""),
+) -> None:
+    """Assigns a list of tags, given their IDs, to an article"""
+    if not BaseInternalApi.subclasses:
+        raise HTTPException(status_code=500, detail="Not implemented")
+    return await BaseInternalApi.subclasses[0]().assign_article_tags(id, id_tags_body)
 
 
 @router.head(
@@ -50,3 +74,47 @@ async def check_article_by_id(
     if not BaseInternalApi.subclasses:
         raise HTTPException(status_code=500, detail="Not implemented")
     return await BaseInternalApi.subclasses[0]().check_article_by_id(id)
+
+
+@router.delete(
+    "/articles/{id}/tags",
+    responses={
+        204: {"description": "No Content, tags unassigned"},
+        400: {"description": "Bad Request, invalid parameters format"},
+        403: {"description": "Forbidden"},
+        404: {"description": "Article Not Found"},
+    },
+    tags=["internal"],
+    summary="Unassign Tags",
+    response_model_by_alias=True,
+)
+async def unassign_article_tags(
+    id: str = Path(..., description=""),
+    ids: list[str] = Query(None, description="List of Tag IDs", alias="ids"),
+) -> None:
+    """Unassigns a list of tags, given their IDs to an article."""
+    if not BaseInternalApi.subclasses:
+        raise HTTPException(status_code=500, detail="Not implemented")
+    return await BaseInternalApi.subclasses[0]().unassign_article_tags(id, ids)
+
+
+@router.put(
+    "/articles/{id}/ratings",
+    responses={
+        204: {"description": "No Content, rating updated"},
+        400: {"description": "Bad Request, invalid parameter format"},
+        401: {"description": "Unauthorized"},
+        404: {"description": "Article Not Found"},
+    },
+    tags=["internal"],
+    summary="Update Rating",
+    response_model_by_alias=True,
+)
+async def update_rating(
+    id: str = Path(..., description=""),
+    id_ratings_body: IdRatingsBody = Body(None, description=""),
+) -> None:
+    """Update the rating of an Article give its unique ID and a rating"""
+    if not BaseInternalApi.subclasses:
+        raise HTTPException(status_code=500, detail="Not implemented")
+    return await BaseInternalApi.subclasses[0]().update_rating(id, id_ratings_body)
