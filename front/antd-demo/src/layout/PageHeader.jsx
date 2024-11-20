2481af47e91e75ac19d53e9235298a1c86024294
adriantd3 <adriantorremocha@hotmail.com>
2024-11-20 11:16:33 +0100

    Header v0.1

---
+++ front/antd-demo/src/layout/PageHeader.jsx
@@ -0,0 +1,75 @@
+import { Layout, Flex, Input, Button, Badge, Avatar } from "antd";
+import {
+  ControlOutlined,
+  PlusOutlined,
+  BellOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import PropTypes from "prop-types";
+import "./PageHeader.css";
+import Title from "antd/es/typography/Title";
+import React from "react";
+
+const FilterIcon = () => {
+  return (
+    <div className="icon-container" onClick={FilterClickHandler}>
+      <ControlOutlined style={{ fontSize: "24px" }} />
+    </div>
+  )
+}
+const AddIcon = () => <PlusOutlined />;
+const BellIcon = () => <BellOutlined />;
+
+const ProfileClickHandler = () => {
+  console.log("Profile clicked");
+};
+
+const NotificationsClickHandler = () => {
+  console.log("Notifications clicked");
+}
+
+const FilterClickHandler = () => {
+  console.log("Filter clicked");
+}
+
+const PageHeader = ({ children }) => {
+  return (
+    <Layout className="app-layout">
+      <Layout.Header className="app-header">
+        <Title style={{ marginTop: "0.5em" }}>LaWiki</Title>
+        <Flex gap={50}>
+          <Input.Search
+            placeholder="search wikis"
+            allowClear
+            suffix={FilterIcon()}
+            size="large"
+            style={{ width: "400px" }}
+          />
+          <Button
+            variant="outlined"
+            icon={AddIcon()}
+            iconPosition="start"
+            size={"large"}
+          >
+            New Wiki
+          </Button>
+          <Badge count={9} size="large">
+            <div className="icon-container" onClick={NotificationsClickHandler}>
+            <BellOutlined style={{ fontSize: "24px" }} />
+            </div>
+          </Badge>
+          <div className="icon-container" onClick={ProfileClickHandler}>
+            <Avatar size="large" icon={<UserOutlined/>}/>
+          </div>
+        </Flex>
+      </Layout.Header>
+      <Layout.Content className="app-content">{children}</Layout.Content>
+    </Layout>
+  );
+};
+
+PageHeader.propTypes = {
+  children: PropTypes.node,
+};
+
+export default PageHeader;
