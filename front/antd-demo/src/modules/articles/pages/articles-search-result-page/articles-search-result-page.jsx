6784b8e0616f63c3e8d7c77167f3ea7f152bf5e5
José Ángel Bueno Ruiz <jabr3830@gmail.com>
2024-11-23 10:12:48 +0100

    articles-search-result-page files and folder

---
+++ front/antd-demo/src/modules/articles/pages/articles-search-result-page/articles-search-result-page.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import './articles-search-result-page.css';
+
+const ArticlesSearchResultPage = () => {
+  return (
+    <div>ArticlesSearchResultPage</div>
+  );
+};
+
+export default ArticlesSearchResultPage;
