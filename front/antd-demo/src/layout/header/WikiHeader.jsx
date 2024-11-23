50ecf8887b1b49094406768ae74fca3e1209d379
Ezequiel Sánchez <124664474+ezee-sg@users.noreply.github.com>
2024-11-23 17:29:55 +0100

    Wiki Edit Page - Create and delete tags

--- front/antd-demo/src/layout/header/WikiHeader.jsx
+++ front/antd-demo/src/layout/header/WikiHeader.jsx
@@ -1,5 +1,13 @@
-import { Flex, Input, Button, Badge, Avatar, Popover, Menu } from "antd";
-import React, { useState, useContext } from 'react';
+import {
+  Flex,
+  Input,
+  Button,
+  Badge,
+  Avatar,
+  Popover,
+  Menu,
+} from "antd";
+import React, { useContext, useState } from "react";
 import { ThemeContext } from "../../context/theme-context";
 import {
   ControlOutlined,
@@ -11,46 +19,37 @@ import PropTypes from "prop-types";
 import "./PageHeader.css";
 import Title from "antd/es/typography/Title";
 import { Link } from "react-router-dom";
+
 const FilterIcon = () => {
   return (
     <div className="icon-container" onClick={FilterClickHandler}>
       <ControlOutlined style={{ fontSize: "24px" }} />
     </div>
-  )
-}
-const AddIcon = () => <PlusOutlined />;
-
-const ProfileClickHandler = () => {
-  console.log("Profile clicked");
+  );
 };
+
+const AddIcon = () => <PlusOutlined />;
 
 const NotificationsClickHandler = () => {
   console.log("Notifications clicked");
-}
+};
 
 const FilterClickHandler = () => {
   console.log("Filter clicked");
-}
+};
 
 const LaWikiClickHandler = () => {
   console.log("LaWiki clicked");
-}
+};
 
 const WikiClickHandler = () => {
   console.log("Wiki clicked");
-}
+};
 
 const WikiHeader = () => {
-
   const { colorTheme, toggleTheme } = useContext(ThemeContext);
-  const [theme, setTheme] = useState(colorTheme === 'dark' ? 'Dark' : 'Light');
-
-  const handleThemeChange = (newTheme) => {
-    setTheme(newTheme);
-    toggleTheme();
-  };
-  const [notifications, setNotifications] = useState('After login');
-  const [language, setLanguage] = useState('English');
+  const [notifications, setNotifications] = useState("After login");
+  const [language, setLanguage] = useState("English");
 
   const popoverContent = (
     <Menu className="user-profile-menu">
@@ -58,33 +57,33 @@ const WikiHeader = () => {
         <strong>See profile</strong>
       </Menu.Item>
       <Menu.Divider />
-      <Menu.SubMenu key="2" title={`Theme: ${theme}`}>
-        <Menu.Item key="2-1" onClick={() => handleThemeChange('Light')}>
-          Light
-        </Menu.Item>
-        <Menu.Item key="2-2" onClick={() => handleThemeChange('Dark')}>
-          Dark
+      <Menu.SubMenu key="2" title={`Theme: ${colorTheme === "light" ? "Light" : "Dark"}`}>
+        <Menu.Item
+          key="2-1"
+          onClick={toggleTheme}
+        >
+          {colorTheme === "light" ? "Switch to Dark" : "Switch to Light"}
         </Menu.Item>
       </Menu.SubMenu>
       <Menu.SubMenu key="3" title={`Notifications: ${notifications}`}>
-        <Menu.Item key="3-1" onClick={() => setNotifications('Always')}>
+        <Menu.Item key="3-1" onClick={() => setNotifications("Always")}>
           Always
         </Menu.Item>
-        <Menu.Item key="3-2" onClick={() => setNotifications('After login')}>
+        <Menu.Item key="3-2" onClick={() => setNotifications("After login")}>
           After login
         </Menu.Item>
-        <Menu.Item key="3-3" onClick={() => setNotifications('Never')}>
+        <Menu.Item key="3-3" onClick={() => setNotifications("Never")}>
           Never
         </Menu.Item>
       </Menu.SubMenu>
       <Menu.SubMenu key="4" title={`Language: ${language}`}>
-        <Menu.Item key="4-1" onClick={() => setLanguage('English')}>
+        <Menu.Item key="4-1" onClick={() => setLanguage("English")}>
           English
         </Menu.Item>
-        <Menu.Item key="4-2" onClick={() => setLanguage('Español')}>
+        <Menu.Item key="4-2" onClick={() => setLanguage("Español")}>
           Español
         </Menu.Item>
-        <Menu.Item key="4-3" onClick={() => setLanguage('Français')}>
+        <Menu.Item key="4-3" onClick={() => setLanguage("Français")}>
           Français
         </Menu.Item>
       </Menu.SubMenu>
@@ -95,18 +94,28 @@ const WikiHeader = () => {
     </Menu>
   );
 
-
   return (
     <>
-      <Flex gap='large'>
+      <Flex gap="large">
         <Link to="/">
-          <Title style={{ marginTop: "0.5em" }} onClick={LaWikiClickHandler} className="header-title">LaWiki</Title>
+          <Title
+            style={{ marginTop: "0.5em" }}
+            onClick={LaWikiClickHandler}
+            className="header-title"
+          >
+            LaWiki
+          </Title>
         </Link>
-        {/* Depende de la información que le venga de la ruta */}
         <Title style={{ marginTop: "0.5em" }}>/</Title>
-        <Title style={{ marginTop: "0.5em" }} onClick={WikiClickHandler} className="header-title">JoJoWiki</Title>
+        <Title
+          style={{ marginTop: "0.5em" }}
+          onClick={WikiClickHandler}
+          className="header-title"
+        >
+          JoJoWiki
+        </Title>
       </Flex>
-      
+
       <Flex gap={50}>
         <Input.Search
           placeholder="search for articles"
@@ -125,19 +134,19 @@ const WikiHeader = () => {
         </Button>
         <Badge count={9} size="large">
           <div className="icon-container" onClick={NotificationsClickHandler}>
-          <BellOutlined style={{ fontSize: "24px" }} />
+            <BellOutlined style={{ fontSize: "24px" }} />
           </div>
         </Badge>
         <Popover
-        content={popoverContent}
-        trigger="click"
-        placement="bottomRight"
-        overlayStyle={{ width: 220 }}
-      >
-        <Avatar size="large" icon={<UserOutlined/>}>
-          A
-        </Avatar>
-      </Popover>
+          content={popoverContent}
+          trigger="click"
+          placement="bottomRight"
+          overlayStyle={{ width: 220 }}
+        >
+          <Avatar size="large" icon={<UserOutlined />}>
+            A
+          </Avatar>
+        </Popover>
       </Flex>
     </>
   );
