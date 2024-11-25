bb743779e6c72cb52078535da065d5643e3d697c
Victor Perez <vparmenta@uma.es>
2024-11-25 20:25:03 +0100

    Redesign comment-input according to Figma design

--- front/antd-demo/src/modules/comments/components/comment-input/comment-input.jsx
+++ front/antd-demo/src/modules/comments/components/comment-input/comment-input.jsx
@@ -12,10 +12,7 @@ const CommentInput = ({user}) => {
       <Avatar className='comment-avatar' src={user.image} alt={user.name} />
       <div style={{width: "100%"}}>
         <Space style={{width: "100%", paddingRight: "100px"}} direction="vertical">
-          <Space>
-            <Text strong>{user.name}</Text>
-          </Space>    
-          <TextArea size='large' autoSize = {{minRows: 2, maxRows: 3}}/>
+          <TextArea showCount maxLength={100} size='large' autoSize = {{minRows: 3}}/>
           <Button type='primary'>Subir</Button>
         </Space>
       </div>
