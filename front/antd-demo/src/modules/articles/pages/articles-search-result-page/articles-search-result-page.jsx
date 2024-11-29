2fc39ea4de9a48e6832dd65a836853b877e550ad
JoseBueno30 <124668506+JoseBueno30@users.noreply.github.com>
2024-11-29 10:36:02 +0100

    Search results first version

--- front/antd-demo/src/modules/articles/pages/articles-search-result-page/articles-search-result-page.jsx
+++ front/antd-demo/src/modules/articles/pages/articles-search-result-page/articles-search-result-page.jsx
@@ -4,6 +4,7 @@ import './articles-search-result-page.css';
 import { Flex, Row, Col, Button } from 'antd';
 import { useSearchParams, useNavigate } from "react-router-dom";
 import ArticleList from '../../components/article-list/article-list';
+import axios from "axios";
 
 const articleList = [
   {
@@ -100,13 +101,57 @@ const articleList = [
   },
 ]
 
+const wiki_id = "672c8721ba3ae42bd5985361"
+
 const ArticlesSearchResultPage = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const [loading, setLoading] = useState(false);
+  const [articles, setArticles] = useState([]);
+
+  // TODO: In order to search in a given wiki, should the gateway provide an endpoint to search having only the wiki name? or should the front handle this
+
+  const searchArticles = async () => {
+    setLoading(true);
+    try{
+      var params = {
+        wiki_id: wiki_id,
+        name: searchParams.get("name") || "",
+        tags: searchParams.getAll("tags"),
+        offset: 0,
+        limit: 10,
+        order: searchParams.get("order") || "",
+        creation_date: searchParams.get("creation_date") || "",
+        author_name: searchParams.get("author_name") || "",
+        editor_name: searchParams.get("editor_name") || "",
+        lan: "es",
+      }
+
+      params = Object.fromEntries(
+        Object.entries(params).filter(([_, value]) => value && value.length !== 0)
+      );
+
+      const response = await axios.get("http://localhost:3000/v1/articles",{params:params})  
+
+      setArticles(response.data.articles)
+
+    } catch (err){
+      console.log(err);
+    }finally{
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    setLoading(true);
+    searchArticles();
+  }, [wiki_id, searchParams])
+
+
   return (
     <Flex vertical align="center" style={{minWidth:400}}>
       <h2 className='article-search-results-title'>Search results for: (article_name)</h2>
       <h3 className='article-search-results-info'>Total: (X) | Filters: (used_filters)</h3>
-      <ArticleList articleList={articleList}/>
+      <ArticleList articleList={articles}/>
       <Row align="middle" justify="space-around" style={{ width:"80%", marginTop:20}}>
         <Col>
           <Button type="primary">Previous</Button>
