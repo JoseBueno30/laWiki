0bcf5555df52ceff58ece8c7f99cd7e446b83756
adriantd3 <adriantorremocha@hotmail.com>
2024-12-01 16:53:18 +0100

    WIkiContext created
    It manages a useState variable that contains the wiki_name (the url wiki name) and the wiki_info which is the Wiki object itself.

--- front/antd-demo/src/modules/wiki/service/wiki-service.jsx
+++ front/antd-demo/src/modules/wiki/service/wiki-service.jsx
@@ -1,12 +1,35 @@
+import { useContext } from "react";
 import apiClient from "../../../interceptor/interceptor";
+import { SettingsContext } from "../../../context/settings-context";
 
 const WikiService = () => {
-    
-    const getRatedWikis = async () => {};
-  
-    return {
-        getRatedWikis,
-    };
+  const getWiki = async (wiki_name, locale) => {
+    try {
+      const params = new URLSearchParams({ lang: locale });
+      const url = `/v1/wikis/${wiki_name}?${params.toString()}`;
+
+      const response = await apiClient.get(url);
+      return response;
+    } catch (error) {
+      console.error("WikiService.getWiki:", error);
+      return Promise.reject(error);
+    }
   };
-  
-  export default WikiService;
+
+  const getRatedWikis = async () => {
+    try {
+      const response = await apiClient.get("/wikis");
+      return response;
+    } catch (error) {
+      console.error("WikiService.getRatedWikis:", error);
+      return Promise.reject(error);
+    }
+  };
+
+  return {
+    getWiki,
+    getRatedWikis,
+  };
+};
+
+export default WikiService;
