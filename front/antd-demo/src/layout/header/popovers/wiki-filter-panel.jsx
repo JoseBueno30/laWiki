4af3670bd2d772b9e284f5de1b61b20bfcd03e84
adriantd3 <adriantorremocha@hotmail.com>
2024-12-01 22:28:23 +0100

    Header search inputs are functional
    - Header landmarks redirect to the corresponding pages
    
    -search inputs work and redirect to the corresponding route with the search params

--- front/antd-demo/src/layout/header/popovers/wiki-filter-panel.jsx
+++ front/antd-demo/src/layout/header/popovers/wiki-filter-panel.jsx
@@ -1,76 +1,90 @@
 import {
-    Divider,
-    Flex,
-    Radio,
-    Space,
-    Tag,
-    Select,
-    DatePicker,
-    Input,
-  } from "antd";
-  const { RangePicker } = DatePicker;
-  import { useTranslation } from "react-i18next";
-  import dayjs from "dayjs";
-  import customParseFormat from "dayjs/plugin/customParseFormat";
-  dayjs.extend(customParseFormat);
-  
-  const dateFormat = "YYYY/MM/DD";
-  
-  const WikiFilterPanel = ({ filters, setFilters }) => {
-    const { t } = useTranslation('header');
+  Divider,
+  Flex,
+  Radio,
+  Space,
+  Tag,
+  Select,
+  DatePicker,
+  Input,
+} from "antd";
+const { RangePicker } = DatePicker;
+import { useTranslation } from "react-i18next";
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+dayjs.extend(customParseFormat);
 
-    const orderOptions = [
-      { label: t('order-recent'), value: "recent" },
-      { label: t('order-oldest'), value: "oldest" },
-      { label: t('order-popular'), value: "popular" },
-      { label: t('order-unpopular'), value: "unpopular" },
-    ];
-    
-    const updateOrder = (e) => {
-      setFilters({ ...filters, order: e.target.value });
-    };
+const dateFormat = "YYYY/MM/DD";
 
-    const updateDateRange = (_, dateStrings) => {
+const WikiFilterPanel = ({ filters, setFilters }) => {
+  const { t } = useTranslation("header");
+
+  const orderOptions = [
+    { label: t("order-recent"), value: "recent" },
+    { label: t("order-oldest"), value: "oldest" },
+    { label: t("order-popular"), value: "popular" },
+    { label: t("order-unpopular"), value: "unpopular" },
+  ];
+
+  const updateOrder = (e) => {
+    setFilters({ ...filters, order: e.target.value });
+    console.log(filters.startDate, filters.endDate);
+  };
+
+  const updateDateRange = (dates) => {
+    if (!dates) {
       setFilters({
         ...filters,
-        startDate: dateStrings[0] ? dateStrings[0] : null,
-        endDate: dateStrings[1] ? dateStrings[1]: null,
+        startDate: null,
+        endDate: null,
       });
-    };
-  
-    const updateAuthor = (e) => {
-      setFilters({ ...filters, author: e.target.value });
-    };
-  
-    return (
-      <Flex vertical align="center">
-        <Divider>{t('order-divider')}</Divider>
-        <Radio.Group
-          optionType="button"
-          options={orderOptions}
-          value={filters.order}
-          onChange={updateOrder}
-          block
-        />
-        <Divider>{t('daterange-divider')}</Divider>
-        <RangePicker
-          value={[
-            filters.startDate ? dayjs(filters.startDate, dateFormat) : null,
-            filters.endDate ? dayjs(filters.endDate, dateFormat) : null,
-          ]}
-          onChange={updateDateRange}
-        />
-        <Divider>{t('author-divider')}</Divider>
-        <Input
-          defaultValue={filters.author}
-          placeholder={t('author-placeholder')}
-          allowClear
-          onChange={updateAuthor}
-          style={{ width: "80%" }}
-        />
-      </Flex>
+      return;
+    }
+
+    const formattedDates = dates.map((date) =>
+      date ? dayjs(date).format("YYYY/MM/DD") : null
     );
+    setFilters({
+      ...filters,
+      startDate: formattedDates[0],
+      endDate: formattedDates[1],
+    });
   };
-  
-  export default WikiFilterPanel;
-  
+
+  const updateAuthor = (e) => {
+    setFilters({ ...filters, author: e.target.value });
+  };
+
+  return (
+    <Flex vertical align="center">
+      <Divider>{t("order-divider")}</Divider>
+      <Radio.Group
+        optionType="button"
+        options={orderOptions}
+        value={filters.order}
+        onChange={updateOrder}
+        block
+      />
+      <Divider>{t("daterange-divider")}</Divider>
+      <RangePicker
+        allowEmpty={[true, true]}
+        value={[
+          filters.startDate ? dayjs(filters.startDate, "YYYY/MM/DD") : null,
+          filters.endDate ? dayjs(filters.endDate, "YYYY/MM/DD") : null,
+        ]}
+        onChange={updateDateRange}
+        format={dateFormat}
+      />
+      <Divider>{t("author-divider")}</Divider>
+      <Input
+        defaultValue={filters.author}
+        placeholder={t("author-placeholder")}
+        allowClear
+        onChange={updateAuthor}
+        style={{ width: "80%" }}
+      />
+    </Flex>
+  );
+};
+
+export default WikiFilterPanel;
