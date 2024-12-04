a0da7c3abcd6e0c0a5abd74d37c8d1c2e5251e82
adriantd3 <adriantorremocha@hotmail.com>
2024-12-04 23:18:45 +0100

    Loading state when uploading comment

--- front/antd-demo/src/modules/comments/components/comment-input/comment-input.jsx
+++ front/antd-demo/src/modules/comments/components/comment-input/comment-input.jsx
@@ -4,15 +4,19 @@ import './comment-input.css';
 import '../comment/comment.css'
 import { Avatar, Flex, Input, Space, Button } from 'antd';
 import CommentService from '../../service/comment-service';
+import { useTranslation } from 'react-i18next';
 
 const {TextArea} = Input
 
 const CommentInput = ({user, uploadFunc}) => {
-
+  const { t } = useTranslation();
   const [text, setText] = useState('')
+  const [submitting, setSubmitting] = useState(false)
 
   const uploadComment = async () =>{
+    setSubmitting(true)
     await uploadFunc(text)
+    setSubmitting(false)
     setText('')
   }
 
@@ -22,7 +26,7 @@ const CommentInput = ({user, uploadFunc}) => {
       <div style={{width: "100%"}}>
         <Space style={{width: "100%"}} direction="vertical">
           <TextArea value={text} showCount maxLength={200} size='large' autoSize = {{minRows: 3}} onChange={(e) => {setText(e.target.value)}}/>
-          <Button onClick={uploadComment} type='primary'>Subir</Button>
+          <Button onClick={uploadComment} type='primary' loading={submitting ? true : false} iconPosition='end'>{t('article.post-comment-button')}</Button>
         </Space>
       </div>
     </Flex>
