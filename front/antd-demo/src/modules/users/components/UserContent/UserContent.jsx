ae48a92b720e7aff4fb21047539fb197596c9084
JosePrietoPaez <joseprieto.sergio@gmail.com>
2024-12-19 11:54:11 +0100

    Changed user page wiki logic
    Copied Search page and modified it to match the needs of this page. Original page is not modified

--- front/antd-demo/src/modules/users/components/UserContent/UserContent.jsx
+++ front/antd-demo/src/modules/users/components/UserContent/UserContent.jsx
@@ -1,76 +1,20 @@
-import { Tabs, Tree } from "antd";
-import WikiCardGrid from "../../../wiki/components/wiki-card-grid/wiki-card-grid";
-import { useContext, useEffect, useState } from "react";
+import { Tabs } from "antd";
+import { useContext } from "react";
 import { useTranslation } from "react-i18next";
 import SettingsContext from "../../../../context/settings-context";
 import ArticleService from "../../../articles/service/article-service";
+import UserWikis from "../UserWikis/UserWikis";
 
 const { searchArticlesWithParams } = ArticleService();
 
 //Las wikis funcionan
 //He cambiado de idea de como hacer los articulos un monton de veces
 //Hare que se llame a articles y se obtengan las wikis de los articulos
-const UserContent = ({wikis=[], author_name=""}) => {
+const UserContent = ({author_name=""}) => {
     const { t } = useTranslation();
     const { locale } = useContext(SettingsContext);
-    const [offset, setOffset] = useState(0);
-    const [limit, setLimit] = useState(20);
-    const [order, setOrder] = useState("recent");
-    const [articleTree, setATree] = useState([]);
 
-    console.log(wikis);
-
-    const initTreeData = () => {
-        return wikis.map((wiki, i) => ({
-            "title": wiki.name,
-            "key": i+""
-        }));
-    }
-    
-    const getParams = (key) => {
-        return {
-            "wiki_id": wikis[key].id,
-            "author_name": author_name,
-            "lan": locale,
-            "offset": offset,
-            "limit": limit,
-            "order": order
-        }
-    }
-
-    const getArticles = (key) => {
-        let articles = [];
-        searchArticlesWithParams(getParams(key)).then((response) => {
-            articles = response.articles;
-            setOffset(offset + limit);
-        });
-        console.log(`Articles from ${wikis[key].name}: ` + articles);
-        return articles;
-    }
-
-    const onLoadData = ({ key, children }) =>
-        new Promise((resolve) => {
-            if (children) {
-                resolve();
-                return;
-            }
-            setTimeout(() => {
-                const copy = Array.from(articleTree);
-                const articles = getArticles(key);
-                copy[key] = articles;
-                setATree(copy);
-                resolve();
-            }, 5000);
-        });
-
-    useEffect(() => {
-        setATree(initTreeData());
-
-        console.log(articleTree);
-
-    },[]);
-
-    const content_wikis = <WikiCardGrid wikiList={wikis}/>;
+    const content_wikis = <UserWikis author_name={author_name} search_limit={6}/>;
 
     return(<Tabs
     centered
@@ -81,7 +25,7 @@ const UserContent = ({wikis=[], author_name=""}) => {
         },
         { "label": t("article.articles"),
             "key": 2,
-            "children": <Tree treeData={articleTree} loadData={onLoadData}/>
+            "children": <></>
     }]}/>);
 };
 export default UserContent;
--- front/antd-demo/src/modules/users/pages/UserPage.jsx
+++ front/antd-demo/src/modules/users/pages/UserPage.jsx
@@ -1,28 +1,12 @@
-import { useEffect, useState } from "react";
 import UserTop from "../components/UserTop/UserTop";
 import UserContent from "../components/UserContent/UserContent";
-import WikiService from "../../wiki/service/wiki-service";
-
-const { searchWikisWithParams } = WikiService();
 
 const UserPage = () => {
     let userName = "DefaultAuthor";
-    const [wikis,setWikis] = useState([]);
-
-    const getParams = () => {
-        return { "author_name" : userName }
-    }
-
-    useEffect(() => {
-        const params = getParams();
-        searchWikisWithParams(params).then((response) => {
-            setWikis(response.wikis);
-        });
-    },[]);
 
     return(<div>
         <UserTop user_picture="https://picsum.photos/500" user_name={userName} />
-        <UserContent author_name={userName} wikis={wikis}/>
+        <UserContent author_name={userName}/>
     </div>);
 }
 export default UserPage;
