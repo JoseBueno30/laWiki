8190170491bf6c4c6cb97ddc3c9605685595969c
adriantd3 <adriantorremocha@hotmail.com>
2024-12-21 17:52:57 +0100

    CORS added

--- api/microservices/users/src/openapi_server/main.py
+++ api/microservices/users/src/openapi_server/main.py
@@ -15,6 +15,7 @@
 from fastapi import FastAPI
 import firebase_admin
 from firebase_admin import credentials
+from starlette.middleware.cors import CORSMiddleware
 
 from openapi_server.apis.v1_internal_api import router as V1InternalApiRouter
 from openapi_server.apis.v1_public_api import router as V1PublicApiRouter
@@ -27,6 +28,14 @@ app = FastAPI(
 
 cred = credentials.Certificate("./firebase-admin.json")
 firebase_admin.initialize_app(cred)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir todas las orígenes, puedes especificar una lista de orígenes permitidos
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos HTTP
+    allow_headers=["*"],  # Permitir todos los encabezados
+)
 
 app.include_router(V1InternalApiRouter)
 app.include_router(V1PublicApiRouter)
