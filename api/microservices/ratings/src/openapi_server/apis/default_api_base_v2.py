c5a17e8f38c870f4e91f08d17251f3371ff1fb35
DavidBuenoC <davidbuenocarmona@gmail.com>
2024-12-23 12:48:38 +0100

    Ratings v2

---
+++ api/microservices/ratings/src/openapi_server/apis/default_api_base_v2.py
@@ -0,0 +1,70 @@
+# coding: utf-8
+
+from typing import ClassVar, Dict, List, Tuple  # noqa: F401
+
+from openapi_server.models.average_rating import AverageRating
+from openapi_server.models.new_rating import NewRating
+from openapi_server.models.rating import Rating
+
+
+class BaseDefaultApiV2:
+    subclasses: ClassVar[Tuple] = ()
+
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        BaseDefaultApiV2.subclasses = BaseDefaultApiV2.subclasses + (cls,)
+    async def delete_rating(
+        self,
+        id: str,
+    ) -> None:
+        """Delete the rating associated with the selected ID"""
+        ...
+
+
+    async def delete_ratings_articles_id(
+        self,
+        id: str,
+    ) -> None:
+        ...
+
+
+    async def edit_article_rating(
+        self,
+        id: str,
+        new_rating: NewRating,
+    ) -> Rating:
+        """Update the value of an already existing Rating"""
+        ...
+
+
+    async def get_article_average_rating(
+        self,
+        id: str,
+    ) -> AverageRating:
+        """Get data about the average rating of the article"""
+        ...
+
+
+    async def get_rating(
+        self,
+        id: str,
+    ) -> Rating:
+        """Get the Rating with the provided ID"""
+        ...
+
+
+    async def get_ratings_bu_user_on_article(
+        self,
+        articleId: str,
+        userId: str,
+    ) -> Rating:
+        ...
+
+
+    async def rate_article(
+        self,
+        id: str,
+        new_rating: NewRating,
+    ) -> Rating:
+        """Create a rating for a given Article"""
+        ...
